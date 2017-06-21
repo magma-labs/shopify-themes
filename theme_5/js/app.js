bfe80430eb2eb2fdece9b00403fa476074d8037a
Ilyeo <jrag.tkd@gmail.com>
2017-06-21 10:20:30 -0500

    Add header mobile and elements

--- theme_5/js/app.js
+++ theme_5/js/app.js
@@ -1,3 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+// $(document).foundation();
+$(window).on('load', function() {
+
+  var menuButton = document.getElementById('menuButton');
+  menuButton.addEventListener('click', function (e) {
+      menuButton.classList.toggle('is-active');
+      e.preventDefault();
+  });
+
+  console.log("Hola vato!");
+  
+});
