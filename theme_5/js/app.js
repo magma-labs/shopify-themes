165ee605e6af58a02183802f68d0f6d986577e46
Ilyeo <jrag.tkd@gmail.com>
2017-07-03 10:15:34 -0500

    Add click event to burger button

--- theme_5/js/app.js
+++ theme_5/js/app.js
@@ -3,18 +3,15 @@
 // $(document).foundation();
 $(window).on('load', function() {
 
+  var mm_nav = $('.mm-nav');
+
   var menuButton = document.getElementById('menuButton');
   menuButton.addEventListener('click', function (e) {
       menuButton.classList.toggle('is-active');
+      mm_nav.toggleClass('show');
       e.preventDefault();
   });
 
   console.log("Hola vato!");
-
-  var menu_button = $('.menu-button');
-
-  menu_button.click(function() {
-    console.log('Hello click');  
-  });
 
 });
