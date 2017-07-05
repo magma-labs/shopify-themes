e017389c78f299d42c4830eac1ea2d068b1290a1
Ilyeo <jrag.tkd@gmail.com>
2017-07-05 17:52:16 -0500

    Add item classes

--- theme_5/js/app.js
+++ theme_5/js/app.js
@@ -9,6 +9,8 @@ $(window).on('load', function() {
   var bodyTag = $('body');
   var header = $('.main-header');
   var logo = $('.logo');
+  var itemDelete = $('.item-delete');
+  var itemPlus = $('.item-plus');
 
   var menuButton = document.getElementById('menuButton');
   menuButton.addEventListener('click', function (e) {
@@ -19,6 +21,20 @@ $(window).on('load', function() {
       logo.toggleClass('mobile-logo-one');
       logo.toggleClass('mobile-logo-two');
       e.preventDefault();
+  });
+
+  itemDelete.click(function() {
+    console.log('Delete action.');
+    $('#item01').remove();
+  });
+
+  itemPlus.click(function() {
+    var count = parseInt($(this).text());
+    count = count + 1;
+    $(this).text(count);
+    $(this).append('<span class="triangle"></span>');
+    console.log('Add action');
+    console.log(count);
   });
 
   console.log("Hola vato!");
