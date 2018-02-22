c1a375377927912333cd74d1b7a12c04f7331b7d
luisarmandodv <luisarmandodv@gmail.com>
2018-02-22 16:43:54 -0600

    Additon of Company Logo

--- MH-FiorellaTheme5/source/javascripts/site.js
+++ MH-FiorellaTheme5/source/javascripts/site.js
@@ -1,8 +1,12 @@
 // This is where it all goes :)
 function openNav() {
     document.getElementById("mySidenav").style.width = "95%";
+    document.getElementById("logo").src = "../images/home/logo_fiorella_menu.png";
+    document.getElementById("logo").style.zIndex = "10";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("logo").src = "../images/home/wlogo_fiorella_mobil.png";
+    document.getElementById("logo").style.zIndex = "2";
 }
