36c58415d404355e094d57ad7f35b3539133c0b2
Jaime <j.loyolarangel@gmail.com>
2018-02-23 11:58:14 -0600

    Add slider feature

---
+++ theme6/source/javascripts/featured_products.js
@@ -0,0 +1,15 @@
+var images = ["bear_toy", "train_toy", "plane_toy", "ship_toy"];
+var i = 0;
+
+function changeImage(value) {
+  i += value;
+
+  if (i < 0) {
+    i = images.length - 1;
+  }
+  else if (i > images.length - 1) {
+    i = 0;
+  }
+
+  document.getElementById("featuredToy").src = "images/" + images[i] + ".png";
+}
