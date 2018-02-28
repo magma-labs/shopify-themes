90ef07d2471c78c9c5603b9f3564982d99556903
hrodriguez1 <hector.m.rm@hotmail.com>
2018-02-28 13:43:40 -0600

    Adds content at banner

--- theme6/source/javascripts/site.js
+++
@@ -1,9 +0,0 @@
-// This is where it all goes :)
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "90%";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
