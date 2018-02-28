230fa288aca4bc0109fddd023679f7fb22bc392b
Jaime <j.loyolarangel@gmail.com>
2018-02-28 11:06:23 -0600

    Add feature slider

---
+++ theme6/source/javascripts/related_products.js
@@ -0,0 +1,27 @@
+
+var images = ['bear_toy', 'train_toy', 'plane_toy', 'ship_toy'];
+var i = 0;
+
+function changeImage(value) {
+  i += value;
+
+  if (i < 0) {
+    i = images.length - 1;
+  }
+  else if (i > images.length - 1) {
+    i = 0;
+  }
+
+  document.getElementById('featuredToy').src = 'images/' + images[i] + '.png';
+}
+
+const leftArrow = document.getElementById('leftArrow');
+const rightArrow = document.getElementById('rightArrow');
+
+leftArrow.addEventListener('click', () => {
+  changeImage(-1);
+});
+
+rightArrow.addEventListener('click', () => {
+  changeImage(1);
+});
