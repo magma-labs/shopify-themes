7e3e7432dd4f6a441d7ba77ef7cb9b93a2d66c66
hrodriguez1 <hector.m.rm@hotmail.com>
2018-03-26 16:56:21 -0600

    applies corrections on lider menu

--- Lil-Peep/src/scripts/theme.js
+++ Lil-Peep/src/scripts/theme.js
@@ -52,3 +52,11 @@ $(document).ready(function() {
     document.documentElement.className = document.documentElement.className.replace('supports-no-cookies', 'supports-cookies');
   }
 });
+
+//menu slider (ojala)
+
+    $(document).ready(function() {
+    $('#qwerty').click(function() {
+            $('#mySidenav').toggle("slide");
+    });
+});
